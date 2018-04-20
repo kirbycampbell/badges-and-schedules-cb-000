41ceb2c48f03495436a7d6c37f48bcd615c4acf6
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-20 18:32:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,17 @@
 #names = ["Arel"]
 
-def badge_maker(names)
-  names.each do |name|
+def badge_maker(attendees)
+  attendees.each do |name|
     puts "Hello, my name is #{name}."
   end
 end
-names = ["Sunny", "Joe"]
+#names = ["Sunny", "Joe"]
 #def batch_badge_creator(names)
 #  names.each do |name|
 #    puts "Hello, my name is #{name}."
 # end
 #end
-badge_maker("Arel")
+#badge_maker("Arel")
 #def assign_rooms(names)
 #  i == 0
 #  names.each do |name|
