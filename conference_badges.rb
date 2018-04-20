e8024b0b067aa274a4205af93a9e2e249f6a8da8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-20 18:49:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,10 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
- attendees.each do |i|
-   counter = 0
-   counter += 1
-   i == counter
-    puts "You'll be assigned to room #{i}!"
+ attendees.collect do |name|
+    puts "Hello my name is #{}! You'll be assigned to room #{i}!"
   end
 end
